0a7c722b09123eaabdecad8bce8af994273f6d0b
Matthias Killat <matthias.killat{at}gmx.de>
2021-11-11 12:46:39 +0100

    update interface description

--- include/exchange_buffer_interface.hpp
+++ include/exchange_buffer_interface.hpp
@@ -3,20 +3,23 @@
 #include <optional>
 
 template <class T> class ExchangeBuffer {
-
-  // TODO: doxygen
-  // write and discard old value if any
-  // returns true if successful false otherwise (leaves buffer unchanged)
-  // exercise: variant where write returns the old value
+  /// @brief write new value and discard old value if any
+  /// @param value value to be written
+  /// @return true if successful false otherwise (leaves buffer unchanged)
+  /// @note should only be unsuccessful if the memory (usable by the buffer) is
+  /// exhausted
   virtual bool write(const T &value) = 0;
 
-  // write value if buffer is empty
-  // return true if successful, false otherwise
+  /// @brief write new value if buffer is empty
+  /// @param value value to be written
+  /// @return true if successful false otherwise (leaves buffer unchanged)
   virtual bool try_write(const T &value) = 0;
 
-  // take value from buffer (empty afterwards)
+  /// @brief remove data from buffer and return it
+  /// @return value removed or nullopt if there was no value
   virtual std::optional<T> take() = 0;
 
-  // read value from buffer (value will still be in the buffer)
+  /// @brief read data in buffer and return it
+  /// @return value read or nullopt if there was no value
   virtual std::optional<T> read() = 0;
 };
